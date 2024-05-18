b7f84c5bb5b3c0ab3fb3a4690a9c4eabb6154c43
Srg206 <Srg206.32@gmail.com>
2024-05-18 20:35:02 +0300

    upd

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from src.connection import *
 from sqlalchemy import String, create_engine, text
 from src.config import Postgres_asyncpg_URL, Postgres_psycopg_URL
 from src.models.models import User
-from src.schemes.schemes import CreateUser, LoginUser
 from src.utils.utils import encode_password,verify_password
 from datetime import datetime
 from typing import List, Union
@@ -19,41 +18,12 @@ from dotenv import load_dotenv
 import os
 from src.utils.utils import *
 from src.work_with_info.router import info_router
+from src.auth.router import *
 
 
 
 app=FastAPI()
 
 app.include_router(info_router)
-
-
-
-@app.post("/create_user")
-def login(user_data: CreateUser):
-    print("create_user")
-    Session = sessionmaker(bind=sync_engine)
-    session = Session()
-    if (session.query(User).filter_by(email=user_data.email).first() is None):
-        hashed_password = encode_password(user_data.password)
-        new_record = User(name=user_data.username, email=user_data.email, password=hashed_password)
-        session.add(new_record)
-        session.commit()
-        session.close()
-        return {"access_token": create_jwt_token({"sub": user_data.email})}
-    else:
-        return{"error": "Invalid credentials"}
-
-        
-
-@app.post("/login")
-def login(user_data: LoginUser):
-    Session = sessionmaker(bind=sync_engine)
-    session = Session()
-    found_user=session.query(User).filter_by(email=user_data.email).first() 
-    print(user_data.password)
-    if (found_user is not None) and verify_password(user_data.password,found_user.password):
-        return {"access_token": create_jwt_token({"sub": user_data.email})}
-    else:
-        return{"error": "Invalid credentials"}
-    
+app.include_router(auth_router)
 
---
+++ src/auth/router.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Body, Depends
+from pydantic import BaseModel
+from ..connection import *
+from sqlalchemy import select, insert
+from ..models.models import User
+from ..utils.utils import *
+from .schemes.schemes import CreateUser, LoginUser
+auth_router = APIRouter(
+    prefix="/auth",
+    tags=["auth"]
+)
+
+
+
+
+@auth_router.post("/create_user")
+def login(user_data: CreateUser):
+    print("create_user")
+    Session = sessionmaker(bind=sync_engine)
+    session = Session()
+    if (session.query(User).filter_by(email=user_data.email).first() is None):
+        hashed_password = encode_password(user_data.password)
+        new_record = User(name=user_data.username, email=user_data.email, password=hashed_password)
+        session.add(new_record)
+        session.commit()
+        session.close()
+        return {"access_token": create_jwt_token({"sub": user_data.email})}
+    else:
+        return{"error": "Invalid credentials"}
+
+        
+
+@auth_router.post("/login")
+def login(user_data: LoginUser):
+    Session = sessionmaker(bind=sync_engine)
+    session = Session()
+    found_user=session.query(User).filter_by(email=user_data.email).first() 
+    print(user_data.password)
+    if (found_user is not None) and verify_password(user_data.password,found_user.password):
+        return {"access_token": create_jwt_token({"sub": user_data.email})}
+    else:
+        return{"error": "Invalid credentials"}
